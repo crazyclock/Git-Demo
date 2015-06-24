2a103a02a6bc8a15042e74eb49cb063ad83573dc
crazyclock <shootingguide@126.com>
2015-06-24 14:56:26 +0800

    update  java file

--- src/com/crazyclock/helloGit/HelloGit.java
+++ src/com/crazyclock/helloGit/HelloGit.java
@@ -3,6 +3,6 @@ package com.crazyclock.helloGit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HeeloGit");
-		System.out.println("TEST");
+		System.out.println("HeeloGit01");
 	}
 }
